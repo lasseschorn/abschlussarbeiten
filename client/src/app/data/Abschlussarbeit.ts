718d4170e52e8825c8668e5ad24140c8d49f267f
unknown <lasseschorn>
2019-04-04 12:15:20 +0200

    Data Abschlussarbeit.ts

--- client/src/app/data/Abschlussarbeit.ts
+++ client/src/app/data/Abschlussarbeit.ts
@@ -1,16 +1,30 @@
+import { Kategorie } from './Kategorie';
 import { Student } from './Student';
 import { Dozent } from './Dozent';
 import { Betreuer } from './Betreuer';
 
+
 export class Abschlussarbeit {
-
-arbeitsID: number;
-titel: string;
-datum: Date;
-beschreibung: string;
-abstract: string;
-student: Student;
-dozent: Dozent;
-betreuer: Betreuer;
-
+  abstract: string;
+  titel: string;
+  datum: string;
+  kategorieID: number;
+  kategorie: string;
+  studiengangID: number;
+  studiengang: string;
+  dozentID: number;
+  dozvorname: string;
+  doznachname: string;
+  unternehmensID: string;
+  firmenname: string;
+  adressID: number;
+  strasse: number;
+  hausnummer: string;
+  zusatz: string;
+  plz: number;
+  ort: string;
+  studentID: number;
+  stuvorname: string;
+  stunachname: string;
+  arbeitsID: number;
 }
