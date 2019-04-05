b7e12393cb6f44244449a86accd486cae068343d
unknown <lasseschorn>
2019-04-05 10:48:48 +0200

    admin guard

--- client/src/app/admin.guard.ts
+++ client/src/app/admin.guard.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, CanActivateChild, CanLoad, Route} from '@angular/router';
+import { UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import {map } from 'rxjs/operators';
-
+import { AuthService } from './service/auth.service';
+import { UserService } from './service/user.service';
+import { Router } from '@angular/router';
 @Injectable({
   providedIn: 'root'
 })
 export class AdminGuard implements CanActivate {
+  constructor(  private authService: AuthService,
+    private router: Router,
+    private userService: UserService ) {
+}
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
